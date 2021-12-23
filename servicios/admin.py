f097df2c68df06c0fd1e375e5ecd10e46e077c7a
leonahuelfrias <leonahuelfrias@gmail.com>
2021-12-23 10:17:25 -0300

    agregando imagenes en servicios

--- servicios/admin.py
+++ servicios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+# importo servicio
+from .models import Servicio
 # Register your models here.
+
+class Servicio_admin(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+# registro en el admin
+admin.site.register(Servicio, Servicio_admin)
--- servicios/models.py
+++ servicios/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Servicio(models.Model):
     titulo = models.CharField(max_length=50)
     contenido = models.CharField(max_length=50)
-    imagen = models.ImageField() # debo instalar Pilow
+    # debo instalar Pilow
+    imagen = models.ImageField(upload_to='servicios') #crea una carpeta dentro de media y guarda las imagenes
     created = models.DateTimeField(auto_now_add = True) # crea la fecha automaticamente
     updated = models.DateTimeField(auto_now_add = True)
 
